bda236ed89a3a3cbdf1d2219debdd974db753016
Bohdan Tsaruk <iamtsarukbohdan@gmail.com>
2023-06-25 19:59:17 +0200

    HW7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const collection = document.querySelector('.gallery');
+function createGalerryMarkup(arr){
+    return arr.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+}
+
+// const galleryItemsMarkup = galleryItems.map(item => createGalerryMarkup(item)).join('');
+
+
+
+collection.insertAdjacentHTML('afterbegin', createGalerryMarkup(galleryItems));
+collection.addEventListener('click', handlerImg) 
+
+function handlerImg(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('gallery__item')) {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" alt="${evt.target.description}" width="1300" height="900" data-source="${evt.target.dataset.source}">`, {}
+    );
+    instance.show();   
+    document.body.addEventListener('keydown', (evt) => {
+  if (evt.key === "Escape") instance.close()
+})
+};
+
+
+
+
+// function handlerEscModal(evt) {
+//     if (evt.code === "Escape") {
+//         instance.close(evt)
+//     }
+// }
+
+
+// window.addEventListener('keydown', handlerEscModal)
+// window.removeEventListener('keydown', handlerEscModal)
+
+
