12d148c14cc6b5e25ebe034b6990204ffcda92fa
Bohdan Tsaruk <iamtsarukbohdan@gmail.com>
2023-06-26 15:33:30 +0200

    HW7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const collection = document.querySelector('.gallery');
+function createGalerryMarkup(arr){
+    return arr.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`).join('')
+}
+
+collection.insertAdjacentHTML('afterbegin', createGalerryMarkup(galleryItems));
+collection.addEventListener('click', handlerImg);
+
+
+function handlerImg(evt) {
+    evt.preventDefault();
+    // if (evt.target.classList.contains('gallery__item')) {
+    //     return;
+    // }
+    let lightbox = new SimpleLightbox('.gallery a',
+        { captionsData: 'alt', captionDelay: 250 });
+    
+    document.body.addEventListener('keydown', (evt) => {
+  if (evt.key === "Escape") instance.close()
+})
+
+//    const instance = basicLightbox.create(
+//   `
+//     <img width="1300" height="auto" src="">`,
+//   {
+//     onShow: (instance) => {
+//       window.addEventListener('keydown', onEscKeyPress);
+//     },
+//     onClose: (instance) => {
+//       window.removeEventListener('keydown', onEscKeyPress);
+//     },
+//   }
+// );
+};
+
